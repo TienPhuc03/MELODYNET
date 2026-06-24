8c1f9bf69cc0e41bad6f71eaf4bd95b718c0d378
TienPhuc03 <nguyentrantienphuc03082006@gmail.com>
2026-06-25 00:34:25 +0700

    Add database models for User, Song, and ListeningHistory; enhance DB health check

--- server/api/main.py
+++ server/api/main.py
@@ -1,11 +1,14 @@
 import sys
 from sqlalchemy import text
-from server.db.database import engine
+from server.db.database import engine,Base
+from server.db.models import *
 
 # hàm kiểm tra tình trạng db 
 def check_db():
     print ("Melodynet Database - Check Healthy")
     try:
+        # quét và sinh db nếu chưa có
+        Base.metadata.create_all(bind=engine)
         with engine.connect() as conn:
             result = conn.execute(text("SELECT 1"))
             print("Ket noi csdl thanh cong", result.scalar())
--- server/db/models.py
+++ server/db/models.py
@@ -1,1 +1,28 @@
-"""Database models placeholder for MELODYNET users, songs, and history."""
+# -- Active: 1782322337824@@127.0.0.1@5432@melodynet_db
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
+from sqlalchemy.sql import func
+from server.db.database import Base
+
+
+class User(Base):
+    __tablename__ = "users"
+    id = Column(Integer, primary_key=True, index=True)
+    username = Column(String(50), unique=True, nullable=False, index=True)
+    password = Column(String(255), nullable=False)  # lưu chuỗi mật khẩu đã hashed
+
+class Song(Base):
+    __tablename__ = "songs"
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String(100), nullable=False, index=True)  # tên bài hát (được index để search nhanh)
+    artist = Column(String(100), nullable=True)             # tên ca sĩ / nghệ sĩ
+    file_path = Column(String(255), nullable=False)          # đường dẫn tuyệt đối tới file .mp3 trên server
+
+class ListeningHistory(Base):
+    __tablename__ = "history"
+    id = Column(Integer, primary_key=True, index=True)
+    # khóa ngoại liên kết tới bảng users (biết ai nghe)
+    user_id = Column(Integer, ForeignKey("users.id", ondelete="CASCADE"), nullable=False)
+    # khóa ngoại liên kết tới bảng songs (biết nghe bài gì)
+    song_id = Column(Integer, ForeignKey("songs.id", ondelete="CASCADE"), nullable=False)
+    # mốc thời gian nghe (tự động lấy thời gian thực tại của hệ thống khi ghi log)
+    played_at = Column(DateTime(timezone=True), server_default=func.now())
